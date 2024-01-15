e2ad1bdd7a43843e678ebc55d879e89e8005d0cc
nb-04 <121829108+nb-04@users.noreply.github.com>
2024-01-15 15:27:34 -0600

    Add files via upload

---
+++ heatingcooling.py
@@ -0,0 +1,25 @@
+# Burkamper, Nicholas
+# 1/15/2024
+# Returns the running total of the numbner of heating and cooling days based on user input of average daily temperature
+
+#Intializes the variables
+average_daily_temperature = 0
+heating_days = 0
+cooling_days = 0
+
+#Categorizes a given temperature as either a heating day or cooling day, or none at all
+while average_daily_temperature >= -459: 
+    average_daily_temperature = int(input("Enter the average daily temperature: "))
+    if average_daily_temperature < -459:
+        break
+    elif average_daily_temperature < 60:
+        heating_days+=1
+    elif average_daily_temperature > 80:
+        cooling_days+=1
+    else:
+        heating_days+=0
+        cooling_days+=0
+
+# Displays the number of heating and cooling days to the user
+print("Heating days: ",heating_days)
+print("Cooling days: ",cooling_days)
